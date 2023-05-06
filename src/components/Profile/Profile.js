801ab0db277defc7ca233012841a791d5f812724
HenryKim12 <henrykim966@gmail.com>
2023-05-06 15:10:58 -0700

    draft profile

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import github from "./github.png"
+import linkedin from "./linkedin.png"
+import mail from "./mail.png"
+import "./Profile.css"
+
+const Profile = () => {
+  return (
+    <div className='profile'>
+        <h1>Henry Kim</h1>
+        <h2>Computer Science Major, Year 3</h2>
+        <p>I am looking for a position as a software engineer.</p>
+        <ul className="socials">
+          <li className="social">
+            <a href={"https://github.com/HenryKim12"} target="_blank" rel="noreferrer">
+              <img src={github} alt="github icon" width="20px" height="20px"></img>
+            </a>
+          </li>
+          <li className="social">
+            <a href={"https://www.linkedin.com/in/henry-kim-47889b208/"} target="_blank" rel="noreferrer">
+              <img src={linkedin} alt="linkedin icon" width="20px" height="20px"></img>
+            </a>
+          </li>
+          <li className="social">
+            <a href={"mailto:henryshkim@hotmail.com"} target="_blank" rel="noreferrer">
+              <img src={mail} alt="mail icon" width="20px" height="20px"></img>
+            </a>
+          </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Profile
