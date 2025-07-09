9f56914ebe151bdc859c11936d4ed31c2291103b
HenryKim12 <henrykim966@gmail.com>
2025-07-08 19:32:58 -0700

    added experiences

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,5 +1,29 @@
 const experiences = [
     {
+        role: "Software Developer Intern @ Safe Software",
+        employmentDate: "Sept 2025 - Dec 2025",
+        location: "Surrey, BC",
+        companyLink: "https://www.safe.com/",
+        description: "FME Platform Engine",
+        technologies: ["C++"]
+    },
+    {
+        role: "Software Developer Intern @ Geotab",
+        employmentDate: "May 2025 - Aug 2025",
+        location: "Burnaby, BC",
+        companyLink: "https://www.geotab.com/",
+        description: "Optimization",
+        technologies: ["Go", "C#", "TypeScript"]
+    },
+    {
+        role: "Software Engineer Intern @ Arista Networks",
+        employmentDate: "Jan 2025 - Apr 2025",
+        location: "Vancouver, BC",
+        companyLink: "https://www.arista.com/en/",
+        description: "EOS - Extensible Operating System",
+        technologies: ["C++", "Python", "Linux"]
+    },
+    {
         role: "Undergraduate Teaching Assistant @ UBC",
         employmentDate: "Sept 2024 - Present",
         location: "Vancouver, BC",
