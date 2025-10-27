7c111b85e1c04a958804dca95969d496bf86da14
HenryKim12 <henrykim966@gmail.com>
2025-10-26 18:04:06 -0700

    fix date for TA experience

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -25,7 +25,7 @@ const experiences = [
     },
     {
         role: "Undergraduate Teaching Assistant @ UBC",
-        employmentDate: "Sept 2024 - Present",
+        employmentDate: "Sept 2024 - Dec 2024",
         location: "Vancouver, BC",
         companyLink: "https://ubc.ca",
         description: "Facilitated weekly lab sessions and office hours for Basic Algorithms and Data Structures (CPSC 221), enhancing coding and problem-solving skills for students through practical examples and in-depth solutions",
